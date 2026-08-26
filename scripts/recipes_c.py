8ef1f04c0fa6ab241fd4f4fd9bc9f22c6da9f8ea
Ali <10158936+mosajjal@users.noreply.github.com>
2026-08-26 16:30:23 +1200

    fix: pdns ldns crypto lib, wireshark static wsutil

--- scripts/recipes_c.py
+++ scripts/recipes_c.py
@@ -331,7 +331,7 @@ RECIPES = {
              "curl -fsSL --retry 3 https://www.nlnetlabs.nl/downloads/ldns/ldns-1.8.3.tar.gz | "
              "gunzip -c | tar x -C /ldns-src --strip-components=1"],
         build=["cd /ldns-src && ./configure --prefix=/opt/ldns --disable-shared "
-               "--with-ssl=/usr && make -j$(nproc) && make install && cd /src",
+               "--with-ssl=/usr && make -j$(nproc) LIBS='-lcrypto' && make install && cd /src",
                "autoreconf -fi",
                "./configure LDFLAGS='-static -L/opt/ldns/lib' "
                "CPPFLAGS='-I/opt/ldns/include' LIBS='-lldns -lssl -lcrypto -lz'",
@@ -400,7 +400,7 @@ RECIPES = {
               "libgcrypt-static", "c-ares-dev", "libxml2-dev", "libssh-dev", "libpcap-dev",
               "pcre2-dev", "zlib-static", "speexdsp-dev"],
         build=["cmake -B build -G Ninja -DBUILD_wireshark=OFF -DBUILD_qtapps=OFF "
-               "-DENABLE_PCAP=OFF -DZLIB_INCLUDE_DIR=/usr/include "
+               "-DENABLE_STATIC=ON -DENABLE_PCAP=OFF -DZLIB_INCLUDE_DIR=/usr/include "
                "-DZLIB_LIBRARY=/lib/libz.a -DCMAKE_BUILD_TYPE=Release "
                "-DCMAKE_EXE_LINKER_FLAGS=-static", "cmake --build build -j$(nproc)",
                "for f in tshark dumpcap editcap mergecap capinfos captype rawshark "
